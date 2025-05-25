06091308526c88f6a32b57bc77271ccb909f84eb
Akash_balu <aakashbaluoffical@gmail.com>
2025-05-25 20:20:38 +0530

    Bug fix

--- TaskManagement/core/urls/user_urls.py
+++ TaskManagement/core/urls/user_urls.py
@@ -5,9 +5,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     # path('login',TokenObtainPairView.as_view(),name='user_login'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh',TokenRefreshView.as_view(),name='user_login'),
-    path('register',UserLoginView.as_view(),name='user_login'),
+    path('token/', TokenObtainPairView.as_view(), name='token'),
+    path('token/refresh',TokenRefreshView.as_view(),name='refresh'),
+    path('register',UserLoginView.as_view(),name='register'),
 
 
     path('',UserListView.as_view(),name='user_login'),
